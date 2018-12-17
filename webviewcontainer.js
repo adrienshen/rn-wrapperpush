c7a65df9345349e0e74c28ea842592c0ab565d2d
adrienshen <adrienshen.dev@gmail.com>
2018-12-17 22:11:05 +0800

    Basic web message to upper wrapper comm.

--- webviewcontainer.js
+++ webviewcontainer.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native';
 
+
 class MyInlineWeb extends Component {
   render() {
+    const uri = "https://facebook.github.io/react-native/docs/getting-started";
+    const app = "https://rnw-pn.firebaseapp.com/";
     return (
       <WebView
-        source={{uri: 'https://github.com/facebook/react-native'}}
+        source={{ uri: app }}
         originWhitelist={['*']}
+        injectedJavaScript="window.MOBILE_ENV = 'REACT_ANDROID'"
+        onMessage={(event) => this.props.handleWebViewMessage(event.nativeEvent.data)}
       />
     );
   }
